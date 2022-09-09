f0f501f0d000eb0d56188540a3f220f30f70f8e2
Shatfi Husna <80335904+shatfihusna@users.noreply.github.com>
2022-09-09 13:09:23 +0700

    View Products

--- laravelecommerce/app/Models/Product.php
+++ laravelecommerce/app/Models/Product.php
@@ -30,4 +30,8 @@ class Product extends Model
     {
         return $this->hasMany(ProductImage::class, 'product_id', 'id');
     }
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id', 'id');
+    }
 }
